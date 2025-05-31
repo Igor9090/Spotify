d93cd7734413ce9459ad188ea38a7635d016b145
Igor9090 <igorvinicin123@email.com>
2025-05-30 21:32:47 -0300

    ajustes para o deploy

--- BackEnd/API/server.js
+++ BackEnd/API/server.js
@@ -1,23 +1,33 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors())
+app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api", (request, response) => {
   response.send("Só estamos trabalhando com '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
+
+app.use(express.static(path.join(__dirname, "../../FrontEnd/dist/")));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../../FrontEnd/dist/index.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`O Servidor da ${PORT} está ativo`);
--- FrontEnd/src/assets/API/api.js
+++ FrontEnd/src/assets/API/api.js
@@ -1,6 +1,9 @@
+import "dotenv/config"
 import axios from "axios"
 
-const URL = "http://localhost:3000"
+
+const {NODE_ENV} = process.env;
+const URL = NODE_ENV === "development" ? "http://localhost:3000" : "/api"
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
